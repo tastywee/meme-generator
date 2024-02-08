d1b1dc0443d361a583fc29874b2e7627776fb014
Joshua Masangcay <josh.tanay@gmail.com>
2024-02-08 14:57:05 +0800

    Update Meme.js

--- src/Meme.js
+++ src/Meme.js
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import memesData from "./memesData";
+import { useEffect, useState } from "react";
 
 export default function Meme() {
   const [meme, setMeme] = useState({
@@ -7,6 +6,14 @@ export default function Meme() {
     bottomText: "",
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
+
+  const [allMemes, setAllMemes] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://api.imgflip.com/get_memes`)
+      .then((res) => res.json())
+      .then((data) => setAllMemes(data.data.memes));
+  }, []);
 
   function handleChange(event) {
     console.log(event);
@@ -17,12 +24,9 @@ export default function Meme() {
     }));
   }
 
-  const [allMemeImages, setallMemeImages] = useState(memesData);
-
   function getMemeImage() {
-    const memesArray = allMemeImages.data.memes;
-    const randomNumber = Math.floor(Math.random() * memesArray.length);
-    const url = memesArray[randomNumber].url;
+    const randomNumber = Math.floor(Math.random() * allMemes.length);
+    const url = allMemes[randomNumber].url;
     setMeme((prevState) => {
       return {
         ...prevState,
